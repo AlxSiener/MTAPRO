673dfd2a23f10cfb5df22d97cceeb945859d407b
asiener <siener.alex@icloud.com>
2026-04-28 09:07:19 -0400

    Initial Commit

---
+++ MTAPRO/MTAPROApp.swift
@@ -0,0 +1,17 @@
+//
+//  MTAPROApp.swift
+//  MTAPRO
+//
+//  Created by Alexander Siener on 4/28/26.
+//
+
+import SwiftUI
+
+@main
+struct MTAPROApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
