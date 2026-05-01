6de72e3827cea04a7671a6d1981c214fdddd7c1b
BT_C09_16 <mobileappdev@bths.edu>
2026-05-01 09:30:46 -0400

    func should be a method not top level

--- MTAPRO/TrainNetworkClient.swift
+++ MTAPRO/TrainNetworkClient.swift
@@ -3,11 +3,12 @@ import Foundation
 
 @Observable
 class TrainNetworkClient{
-    
+ 
+    func getTrainData(){
+        let request = URLRequest(url: URL(string: "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-g")!)
+       print(request)
+        
+    }
+
 }
 
-func getTrainData(){
-    let request = URLRequest(url: URL(string: "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-g")!)
-   print(request)
-    
-}
