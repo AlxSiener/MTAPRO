c22774e8af5296cd0ac818667908fa627693e126
asiener <siener.alex@icloud.com>
2026-05-04 08:58:29 -0400

    fixing up network class

--- MTAPRO/TrainNetworkClient.swift
+++ MTAPRO/TrainNetworkClient.swift
@@ -2,12 +2,28 @@
 import Foundation
 
 @Observable
-class TrainNetworkClient{
+class TrainNetworkClient {
+    private let session: URLSession
+    private let baseURL = URL(string: "https://api-endpoint.mta.info")!
     
-}
-
-func getTrainData(){
-    let request = URLRequest(url: URL(string: "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-g")!)
-   print(request)
     
+    init(session: URLSession = .shared, apiKey: String? = nil) {
+        self.session = session
+    }
+    
+    func getTrainData() async throws -> Data {
+        // Construct the full URL
+        let url = baseURL.appendingPathComponent("Dataservice/mtagtfsfeeds/nyct%2Fgtfs-g")
+        var request = URLRequest(url: url)
+        
+        
+        let (data, response) = try await session.data(for: request)
+        
+        // Basic HTTP status validation
+        if let http = response as? HTTPURLResponse, !(200...299).contains(http.statusCode) {
+            throw URLError(.badServerResponse)
+        }
+        
+        return data
+    }
 }
