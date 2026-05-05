801957f543e3093385012cbb84df92af0397a040
asiener <siener.alex@icloud.com>
2026-05-05 09:22:04 -0400

    Unnecessary

--- MTAPRO/TrainLine.swift
+++ MTAPRO/TrainLine.swift
@@ -5,14 +5,14 @@
 //  Created by Alexander Siener on 4/30/26.
 //
 
-import Foundation
-
-struct TrainLine: Codable {
-    let line: String
-    var trains: [Train]
-}
-struct Train: Identifiable, Codable {
-    var id: Int
-    var station: String
-    var time: Int
-}
+//import Foundation
+//
+//struct TrainLine: Codable {
+//    let line: String
+//    var trains: [Train]
+//}
+//struct Train: Identifiable, Codable {
+//    var id: Int
+//    var station: String
+//    var time: Int
+//}
