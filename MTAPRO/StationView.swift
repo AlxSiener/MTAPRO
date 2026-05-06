23c22f4f11776b6c63334db99757f4bfc12c66e0
BT_C09_16 <mobileappdev@bths.edu>
2026-05-06 11:41:25 -0400

    created station view

---
+++ MTAPRO/StationView.swift
@@ -0,0 +1,18 @@
+//
+//  StationView.swift
+//  MTAPRO
+//
+//  Created by Student on 5/6/26.
+//
+
+import SwiftUI
+
+struct StationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StationView()
+}
