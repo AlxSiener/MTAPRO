a63da9fe7aeb0c0fedf96aabbb4ee37eeb485638
BT_C09_16 <mobileappdev@bths.edu>
2026-05-06 11:41:57 -0400

    added functionality to TrainLineStationView to display possible stations for a train line

--- MTAPRO/TrainLineStationView.swift
+++ MTAPRO/TrainLineStationView.swift
@@ -9,13 +9,30 @@ import SwiftUI
 
 struct TrainLineStationView: View {
     let trainLine: TrainLineType
-    let stations: [Station] = []
+    let gStations: [Station] = [
+        Station(name: "Court Sq", line: .G, stopNumber: 0),
+        Station(name: "21st st", line: .G, stopNumber: 1),
+        Station(name: "Greenpoint Av", line: .G, stopNumber: 2),
+        Station(name: "Nassau Av", line: .G, stopNumber: 3),
+        Station(name: "metropolitan-lorimer st", line: .G, stopNumber: 4),
+        Station(name: "broadway", line: .G, stopNumber: 5)
+    ]
     
     var body: some View {
-        Text("\(trainLine)")
+        List {
+            ForEach(gStations) { station in
+                NavigationLink {
+                    StationView()
+                } label: {
+                    Text("\(station.name)")
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    TrainLineStationView(trainLine: .G)
+    NavigationStack {
+        TrainLineStationView(trainLine: .G)
+    }
 }
