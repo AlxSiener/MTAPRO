0aa2b6be88c5c6f909ef0d8e0b042254debb1c49
BT_C09_16 <mobileappdev@bths.edu>
2026-05-06 09:29:17 -0400

    used TrainLine objects to make the list and added fucntionality to pass the line that is clicked on to the next view

--- MTAPRO/TrainLineView.swift
+++ MTAPRO/TrainLineView.swift
@@ -8,24 +8,19 @@
 import SwiftUI
 
 struct TrainLineView: View {
-    let trainlines: [String] = [
-        "A",
-        "C",
-        "F",
-        "G",
-        "L",
-        "N",
-        "Q",
-        "D"
+    let trainlines: [TrainLine] = [
+        TrainLine(name: "A", line: .A),
+        TrainLine(name: "G", line: .G),
+        TrainLine(name: "J", line: .J)
     ]
     var body: some View {
         List {
-            ForEach(trainlines, id: \.self) {train in
+            ForEach(trainlines) {trainLine in
                 NavigationLink {
-                    TrainLineStationView()
+                    TrainLineStationView(trainLine: trainLine.line)
                 } label: {
                     Image(systemName: "flag")
-                    Text("\(train)")
+                    Text("\(trainLine.name)")
                 }
             }
             .onTapGesture {
